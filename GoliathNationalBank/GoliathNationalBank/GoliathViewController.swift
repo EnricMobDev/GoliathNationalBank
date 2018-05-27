4ef8c9f9f96772019e8e32a3a2b6aa46f500ecd4
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-05-27 11:23:35 +0200

    UPDATE: Rename the class with a specific name and create a table with a type of cell

---
+++ GoliathNationalBank/GoliathNationalBank/GoliathViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GoliathViewController.swift
+//  GoliathNationalBank
+//
+//  Created by Enric Pou Villanueva on 27/5/18.
+//  Copyright © 2018 Enric Pou Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class GoliathViewController: UIViewController {
+
+    //MARK: - IBOutlets
+    @IBOutlet weak var transactionTable: UITableView!
+    
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
